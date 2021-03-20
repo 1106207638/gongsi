d05566b96f8f40f2a22e76b539ef3a03c15a9f9e
1106207638 <1106207638@qq.com>
2021-03-20 16:38:02 +0800

    防炸

---
+++ src/views/zbtest/newzbjc.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="searchBar">
+    <a-col :xl="5" :lg="6" :md="8" :sm="24">
+      <a-form-item label="检查方式">
+        <j-dict-select-tag
+          type="list"
+          v-model="queryParam.checkWay"
+          placeholder="请选择报告类型"
+          dictCode="check_way"
+        />
+      </a-form-item>
+    </a-col>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+</style>
